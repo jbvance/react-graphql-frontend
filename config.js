313904e198e43639877753c59c0e8bd4c7d3fb08
jbvance <jbvance@gmail.com>
2019-12-02 16:46:14 -0500

    deleteItem mutation and UI update

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
+//export const endpoint = 'http://port-4444.react-graphql-jbvance924063.codeanyapp.com';
 export const endpoint = `http://localhost:4444`;
 export const perPage = 4;
