f28694eb218687b25237266073bd239a63957fb4
jbvance <jbvance@gmail.com>
2019-12-03 17:24:11 -0500

    password reset flow

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,6 @@
 import Signup from '../components/Signup';
 import Signin from '../components/Signin';
+import RequestReset from '../components/RequestReset';
 import styled from 'styled-components';
 
 const Columns = styled.div`
@@ -11,7 +12,8 @@ const Columns = styled.div`
 const SignupPage = props => (
    <Columns>
       <Signup />
-      <Signin />     
+      <Signin /> 
+      <RequestReset />
    </Columns>
 );
 
