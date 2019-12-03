397cf1bbf916b7617869fc416f0385befb54b289
jbvance <jbvance@gmail.com>
2019-12-03 15:12:37 -0500

    sign in form, custom error handling, and update Nav for login status

--- components/Signin.js
+++ components/Signin.js
@@ -5,9 +5,9 @@ import Form from './styles/Form';
 import Error from './ErrorMessage';
 import { CURRENT_USER_QUERY } from './User';
 
-const SIGNUP_MUTATION = gql`
-  mutation SIGNUP_MUTATION($email: String!, $name: String!, $password: String!) {
-    signup(email: $email, name: $name, password: $password) {
+const SIGNIN_MUTATION = gql`
+  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
+    signin(email: $email, password: $password) {
       id,
       email,
       name
@@ -15,7 +15,7 @@ const SIGNUP_MUTATION = gql`
 }
 `;
 
-class Signup extends Component {
+class Signin extends Component {
   state = {
     email: '',
     name: '',
@@ -28,7 +28,7 @@ class Signup extends Component {
 
   render() {
     return (
-      <Mutation mutation={SIGNUP_MUTATION} variables={this.state} refetchQueries={[ { query: CURRENT_USER_QUERY }]}>
+      <Mutation mutation={SIGNIN_MUTATION} variables={this.state} refetchQueries={[ { query: CURRENT_USER_QUERY }]}>
         {(signup, { error, loading }) => {    
           return (<Form method="POST" onSubmit={async e => {
             e.preventDefault();
@@ -37,21 +37,17 @@ class Signup extends Component {
             this.setState({ name: '', email: '', password: '' });
           }}>
             <fieldset disabled={loading} aria-busy={loading}>
-              <h2>Sign Up for an Account</h2>
+              <h2>Sign In</h2>
               <Error error={error} />
               <label htmlFor="email">
                 Email
                 <input type="email" name="email" placeholder="email" value={this.state.email} onChange={this.saveToState} />
-              </label>
-               <label htmlFor="name">
-                Name
-                <input type="text" name="name" placeholder="name" value={this.state.name} onChange={this.saveToState} />
-              </label>
+              </label>              
               <label htmlFor="password">
                 Password
                 <input type="password" name="password" placeholder="password" value={this.state.password} onChange={this.saveToState} />
               </label>
-              <button type="submit">Sign up</button>
+              <button type="submit">Sign In</button>
             </fieldset>
           </Form>)
     }}
@@ -60,4 +56,4 @@ class Signup extends Component {
   }
 }
 
-export default Signup;
+export default Signin;
--- pages/signup.js
+++ pages/signup.js
@@ -1,18 +1,18 @@
 import Signup from '../components/Signup';
+import Signin from '../components/Signin';
 import styled from 'styled-components';
 
-const columns = styled.div`
+const Columns = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 20px;
 `;
 
 const SignupPage = props => (
-   <columns>
+   <Columns>
       <Signup />
-      <Signup />
-      <Signup />
-   </columns>
+      <Signin />     
+   </Columns>
 );
 
 export default SignupPage;
