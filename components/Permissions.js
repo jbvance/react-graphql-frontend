e82c0e973e1e1a48cf0b17309e45eef08156ee75
jbvance <jbvance@gmail.com>
2019-12-04 17:41:43 -0500

    fix bug in Permissions

--- components/Permissions.js
+++ components/Permissions.js
@@ -1,18 +1,11 @@
+import React, { Component } from 'react';
 import { Query } from 'react-apollo';
-import Error from './ErrorMessage';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 import Table from './styles/Table';
 import SickButton from './styles/SickButton';
 import PropTypes from 'prop-types';
 
-const possiblePermissions = [
-    'ADMIN',
-    'USER',
-    'ITEMCREATE',
-    'ITEMUPDATE',
-    'ITEMDELETE',
-    'PERMISSIONUPDATE'
-];
 
 const ALL_USERS_QUERY = gql`
     query {
@@ -25,31 +18,60 @@ const ALL_USERS_QUERY = gql`
     }
 `;
 
-const Permissions = props => (
-    <Query query={ALL_USERS_QUERY}>
-        {({data, loading, error}) => (
-            <div>
-                <Error error={error} />
-                <div>
-                    <h2>Manage Permissions</h2>
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                                {possiblePermissions.map(permission => <th key={permission}>{permission}</th>)} 
-                                <th>&nbsp;</th>                        
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.users.map(user => <UserPermissions key={user.id} user={user} />)}
-                        </tbody>
-                    </Table>
-                </div>
-            </div>
-        )}
-    </Query>
-);
+const possiblePermissions = [
+    'ADMIN',
+    'USER',
+    'ITEMCREATE',
+    'ITEMUPDATE',
+    'ITEMDELETE',
+    'PERMISSIONUPDATE'
+];
+
+const Center = styled.div`
+  text-align: center;
+`;
+
+const ItemsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+`;
+
+class Items extends Component {
+  render() {
+    return (           
+        <Query
+          query={ALL_USERS_QUERY}                    
+        >
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+            console.log("DATA", data);
+            return (
+               <div>
+                  <h2>Manage Permissions</h2>
+                  <Table>
+                      <thead>
+                          <tr>
+                              <th>Name</th>
+                              <th>Email</th>
+                              {possiblePermissions.map(permission => <th key={permission}>{permission}</th>)} 
+                              <th>&nbsp;</th>                        
+                          </tr>
+                      </thead>
+                      <tbody>
+                        {data.users.map(user => <UserPermissions key={user.id} user={user} />)}
+                      </tbody>
+                  </Table>                
+              </div>
+            )
+          }}
+        </Query>            
+    );
+  }
+}
 
 class UserPermissions extends React.Component {
     static propTypes = {
@@ -101,4 +123,5 @@ class UserPermissions extends React.Component {
     }
 }
 
-export default Permissions;
+export default Items;
+export { ALL_USERS_QUERY};
