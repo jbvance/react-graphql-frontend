191df02a2daeb31ae3bd907862ddca9e45043e62
Jason Vance <jbvance@gmail.com>
2019-12-05 06:13:08 -0600

    add apollo local state query and mutation for cart

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,9 @@
 import Link from 'next/link';
 import styled from 'styled-components';
-import Nav from './Nav';
 import Router from 'next/router';
 import NProgress from 'nprogress';
+import Cart from './Cart';
+import Nav from './Nav';
 
 Router.onRouteChangeStart = () => {
   NProgress.start();
@@ -71,7 +72,7 @@ const Header = () => (
     <div className="sub-bar">
       <p>Search</p>
     </div>
-    <div>Cart</div>
+    <Cart />
   </StyledHeader>
 );
 
