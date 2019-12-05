c34ad0e2a4ea424438b0e61734388de0b0f16bd9
jbvance <jbvance@gmail.com>
2019-12-05 13:27:02 -0500

    create and animate CartCount component

--- components/Nav.js
+++ components/Nav.js
@@ -3,7 +3,14 @@ import { Mutation } from 'react-apollo';
 import NavStyles from './styles/NavStyles';
 import Signout from './Signout';
 import User from './User';
+import CartCount from './CartCount';
 import { TOGGLE_CART_MUTATION } from './Cart';
+
+const getCartCount = cart => {
+  return cart.reduce((tally, cartItem) => {
+    return tally + cartItem.quantity;
+  }, 0);
+}
 
 const Nav = () => (
     <User>
@@ -26,7 +33,10 @@ const Nav = () => (
             <Signout />
             <Mutation mutation={TOGGLE_CART_MUTATION}>
               {(toggleCart) => (
-                <button onClick={toggleCart}> My Cart</button>
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount count={getCartCount(me.cart)}/>
+                </button>
               )}
             </Mutation>
             </>
